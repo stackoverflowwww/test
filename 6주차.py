1d16a42aed7fed381d8f23b6b15e71a5b09eb4a1
choieojin <chldjwls85@naver.com>
2021-06-21 22:53:08 +0900

    테스트

---
+++ 6주차.py
@@ -0,0 +1,14 @@
+import bs4, requests
+
+headers = {'User-Agent':'Not_Crawling X)'}
+
+response = requests.get('http://kin.naver.com',headers=headers).text
+soup = bs4.BeautifulSoup(response,'html.parser')
+
+ranks = soup.select('#rankingChart > ul > li')
+
+with open('naverkin_rank.csv','w') as f:
+    for rank in ranks:
+        num = rank.select_one('span.no').text
+        subject = rank.select_one('a.ranking_title').text
+        f.write(f'{num}, {subject}\n')
